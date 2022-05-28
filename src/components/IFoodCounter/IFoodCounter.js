4f80611f14702eeb174d7db800df8e225b2ba3c9
Paulo Henrique <paulo-henrique-morada@hotmail.com>
2022-05-28 19:54:21 -0300

    Trababalhando com useEffect

--- src/components/IFoodCounter/IFoodCounter.js
+++ src/components/IFoodCounter/IFoodCounter.js
@@ -1,17 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function IFoodCounter(){
 
     const [counter, setCounter] = useState(1);
 
     const aumentarValor = () => { setCounter(counter + 1) }
-    const diminuirValor = () => { if(counter > 0) setCounter(counter - 1) }
+    const diminuirValor = () => { 
+        if(counter > 0) setCounter(counter - 1) 
+    }
+
+    useEffect(() => {
+        document.getElementById("valor").innerHTML = counter * 2;
+    },[counter]);
 
     return(
         <div>
             <button onClick={diminuirValor}> - </button>
             <label>{counter}</label>
             <button onClick={aumentarValor}> + </button>
+            <p id="valor"></p>
         </div>
     );
 }
